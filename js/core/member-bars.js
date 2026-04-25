644b2ea59858226f7b74b7f110b22defad0e9cf9
ANN <ann04371207@gmail.com>
2026-04-25 20:02:29 +0800

    2

--- js/core/member-bars.js
+++ js/core/member-bars.js
@@ -13,7 +13,7 @@
   const memberNames = ["YU", "TOMOYA", "HARU", "SO GEON", "SEITA", "HYUI", "YUKI"];
   const memberImageUrls = [
       "https://cloudflarecnimg.scdn.io/i/69ecacbbba5cc_1777118395.webp",
-      "https://videotourl.com/images/1777117726303-03f850e6-d8be-4837-a548-5f23a488d125.png",
+      "https://i.ibb.co/Lh5RmCvX/m2.png",
       "https://videotourl.com/images/1777117742488-0b15c680-e648-41f9-a510-9a0257c47e4c.png",
       "https://videotourl.com/images/1777117753356-88a776bf-51c0-4853-8539-ffab558a2650.png",
       "https://videotourl.com/images/1777117766678-29e75e5b-8401-4cf8-a5ac-ce9d5af2d7ba.png",
